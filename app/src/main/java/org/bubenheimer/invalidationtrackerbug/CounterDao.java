8c2d5869ac51daebb8bb44ab72a1be29cf3ccb2f
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2020-04-16 15:56:47 -0500

    Add UPDATE statement for demonstrating generated code

--- app/src/main/java/org/bubenheimer/invalidationtrackerbug/CounterDao.java
+++ app/src/main/java/org/bubenheimer/invalidationtrackerbug/CounterDao.java
@@ -2,11 +2,17 @@ package org.bubenheimer.invalidationtrackerbug;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Update;
 
 @Dao
 public abstract class CounterDao {
     @Insert
     abstract void insert(
+            Counter entity
+    );
+
+    @Update
+    abstract void update(
             Counter entity
     );
 }
